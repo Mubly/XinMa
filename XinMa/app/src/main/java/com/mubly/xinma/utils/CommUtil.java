659f9956f6793df976a33f15079c58c25a4dc9ce
liangban <Mubly123@163.com>
2020-07-15 17:22:15 +0800

    log日志页面开发基本完成

--- XinMa/app/src/main/java/com/mubly/xinma/utils/CommUtil.java
+++ XinMa/app/src/main/java/com/mubly/xinma/utils/CommUtil.java
@@ -374,7 +374,7 @@ public class CommUtil {
     }
 
     public static String getTimeHMS(Date date) {//可根据需要自行截取数据显示
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         return format.format(date);
     }
 
